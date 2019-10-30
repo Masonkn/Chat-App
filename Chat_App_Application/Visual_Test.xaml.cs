a04a388ed9459e42373d39472f380ac2b679e193
Jpmcdc <Jpmcdc@users.noreply.github.com>
2019-10-30 13:12:24 -0600

    Added .cs file for Visual_Test

---
+++ Chat_App_Application/Visual_Test.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Chat_App_Application
+{
+    /// <summary>
+    /// Interaction logic for Visual_Test.xaml
+    /// </summary>
+    public partial class Visual_Test : Window
+    {
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnMinim_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void BtnMaxim_Click(object sender, RoutedEventArgs e)
+        {
+            AdjustWindowSize();
+
+        }
+
+        private void topDock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                if (e.ClickCount == 2)
+                {
+                    AdjustWindowSize();
+                }
+                else
+                {
+                    Application.Current.MainWindow.DragMove();
+                }
+        }
+
+        private void AdjustWindowSize()
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+        }
+    }
+}
